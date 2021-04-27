ad04dff12e823b884a4d6a874b5059b060053fbd
socalisaac <iestrada5@saddleback.edu>
2021-04-27 14:03:30 -0700

    Simple login created

--- app/src/main/java/org/csuf/cpsc411/cpsc411project/MainActivity.kt
+++ app/src/main/java/org/csuf/cpsc411/cpsc411project/MainActivity.kt
@@ -3,7 +3,9 @@ package org.csuf.cpsc411.cpsc411project
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +15,16 @@ class MainActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.loginButton)
 
         loginButton.setOnClickListener{
-            val intent = Intent(this, MainMenu::class.java)
-            startActivity(intent)
+
+            println("here1")
+
+            val userName = findViewById<EditText>(R.id.userNameField)
+            val password = findViewById<EditText>(R.id.passwordField)
+
+            if(userName.text.toString() == "Admin" &&  password.text.toString() == "1234"){
+                val intent = Intent(this, MainMenu::class.java)
+                startActivity(intent)
+            }
         }
     }
 
